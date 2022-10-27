30cd21ec7749f6950950490079533625d3fa2b9a
spacecodee <spacecodee@gmail.com>
2022-10-27 16:13:31 -0500

    Starting Project

---
+++ src/main/java/com/spacecodee/library_book_backend/LibraryBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacecodee.library_book_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spacecodee/library_book_backend/LibraryBookBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.spacecodee.library_book_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+}
